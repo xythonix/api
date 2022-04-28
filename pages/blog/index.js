167752d7031e857b77a330ac17f6352eb9c002a8
xythonix <cybersite159@gmail.com>
2022-04-28 08:26:48 -0700

    main

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import useRouter from 'next/router'
-import styles from '../../styles/blog.module.css'
-import Card from './card'
+import styles from '../../styles/card.module.css'
+import { Typography } from '@mui/material'
+import { ChevronRight } from '@mui/icons-material'
+import { Fab } from '@mui/material'
+import style from '../../styles/blog.module.css'
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Head from '../head'
 import Header from '../header'
+import Link  from 'next/link'
 
 export const getStaticProps = async () => {
     const response = await fetch('https://fakestoreapi.com/products')
@@ -14,31 +17,38 @@ export const getStaticProps = async () => {
 
 export default function blog ({ data }) {
 
-    const router = useRouter()
-
-    const getId = (id) => {
-      router.push(`/blog/${id}`)
-    }
-
     return (
         <React.Fragment>
 
           <Head />
           <Header />
 
-          <div className='container' id={styles.main} >
-            <div className='row' id={styles.innerRow} >
+          <div className='container' id={style.main} >
+            <div className='row' id={style.innerRow} >
                 {
                     data.map((element) => {
                       return (
-                        <Card 
-                          key = { element.id }
-                          id = { element.id }
-                          cardTitle = { element.title }
-                          imgSrc = { element.image }
-                          price = { `$${element.price}` }
-                          onClicked = { getId }
-                      />
+                        <div key={ element.id } className='col-lg-3 col-md-5' id={styles.card} >
+                          <div className={styles.heading} >
+                              <Typography variant='h5' className={styles.title} > 
+                                  { element.title }
+                              </Typography>
+                          </div>
+                          <div id={styles.imgDiv} >
+                              <img className={styles.imag} src={ element.image } />
+                          </div>
+                          <div className={styles.bottomContent} >
+                              <Typography variant='h5' className={styles.price} >
+                                  <span className={styles.spann} >Price </span> 
+                                  { element.price }
+                              </Typography>
+                              <Link href={`/blog/${element.id}`} >
+                                <Fab className={styles.btn} size='small' variant='extended' color='primary' >
+                                    See <ChevronRight style={{fontSize:'1.3rem'}} />
+                                </Fab>
+                              </Link>
+                          </div>
+                      </div>
                       )
                     })
                 }
